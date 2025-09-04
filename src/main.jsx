3a43a40a26fd2caa86dfd4b826404422eae2b802
sairam311 <kaminenisairam33@gmail.com>
2025-09-04 13:08:27 +0530

    page realod issue in deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.DEV ? '/' : '/chat-app'}>
     <App />
   </BrowserRouter>,
 )
